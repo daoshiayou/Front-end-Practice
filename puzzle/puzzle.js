d5d72aab140d73c65caed64b006e836beec27636
ChenJ <535699028@qq.com>
2019-06-01 13:30:14 +0800

    add the slide effect

--- puzzle/puzzle.js
+++ puzzle/puzzle.js
@@ -39,6 +39,10 @@ function initialize(puzzleArea) {
     }
     puzzleArr.sort(() => 0.5 - Math.random());
     puzzleArr.forEach(element => puzzleArea.append(element));
+    for (let i = 0; i < 9; ++i) {
+        puzzleArea.children[i].style.left = (i % 3) * 150 + 'px';
+        puzzleArea.children[i].style.top = Math.floor(i / 3) * 150 + 'px';
+    }
 }
 
 function startGame() {
@@ -55,19 +59,19 @@ function pauseGame() {
     // var cover = document.createElement('span');
     // cover.innerHTML='<span style="background-color: while width:450px height:450px">暂停中</span>';
     // if (pause) {
-        //     puzzle.append(cover);
-        // }
+    //     puzzle.append(cover);
+    // }
+}
+
+function restartGame() {
+    initialize(puzzle);
+    time = 0;
+    record.setAttribute('hidden', true);
+    startButtom.removeAttribute('hidden');
+    var del = document.querySelector('#congratulations');
+    if (del) {
+        del.remove();
     }
-    
-    function restartGame() {
-        initialize(puzzle);
-        time = 0;
-        record.setAttribute('hidden', true);
-        startButtom.removeAttribute('hidden');
-        var del = document.querySelector('#congratulations');
-        if(del){
-            del.remove();
-        }
 }
 
 function count() {
@@ -95,10 +99,11 @@ function removePuzzle() {
     if (start && !pause) {
         var index = puzzleArr.indexOf(this);
         var blankInd = puzzleArr.indexOf(blank);
-        // console.log(index+','+blankInd);
         if (blankInd == index + 1 || blankInd == index - 1 || blankInd == index + 3 || blankInd == index - 3) {
             [puzzleArr[index], puzzleArr[blankInd]] = [puzzleArr[blankInd], puzzleArr[index]];
-            puzzleArr.forEach(element => puzzle.append(element));
+            // puzzleArr.forEach(element => puzzle.append(element));
+            [blank.style.top, this.style.top] = [this.style.top, blank.style.top];
+            [blank.style.left, this.style.left] = [this.style.left, blank.style.left];
             check();
         }
     }
@@ -111,7 +116,7 @@ function check() {
     if (checkArr.join('-') == '1-2-3-4-5-6-7-8-') {
         congra = document.createElement('p');
         congra.innerHTML = '恭喜你完成了这个无聊的游戏！';
-        congra.setAttribute('id','congratulations');
+        congra.setAttribute('id', 'congratulations');
         start = false;
         congra.style.color = 'red';
         record.firstElementChild.append(congra);
